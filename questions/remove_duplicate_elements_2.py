337891650e5beacbd961811a341d5f35284809d9
Axolotlsfirstlillilypad <133133125+Axolotlsfirstlillilypad@users.noreply.github.com>
2023-09-15 21:22:57 +0800

    Update remove_duplicate_elements_2.py

--- questions/remove_duplicate_elements_2.py
+++ questions/remove_duplicate_elements_2.py
@@ -40,5 +40,41 @@ Tags:
 
 
 def remove_groups(s: str, k: int) -> str:
-    # TODO: Implement the function
-    return ""
+    record=[]
+    i=0
+    #checks if all indices are the same letter, then we really do not need to even process. If the length is not the same return 
+    #the string
+    if s == s[0]*len(s) and len(s)!=k:
+        s=s
+    else:
+        while i < len(s):
+            #must append list beforehand so that the latest iteration's character is recorded in the list,
+            #otherwise the list will not be updated before processing
+            record.append(s[i])
+            if i<len(s)-1:
+                if s[i+1]!= s[i] :
+                    if len(record)==k:
+                        #for some reason this avoids index error
+                        s=s.replace(s[i]*k,"",1)
+                        i=-1
+                    record=[]
+                    # case below needed to explain what happens when the triplet contains the very last index
+            elif i==len(s)-1:
+                if len(record)==k :
+                        s=s.replace(s[i]*k,"",1)
+                        i=-1
+                        record=[]
+            #what to do if there is nothing left in s to return  e.g"aabbcc"
+            if len(s)==0:
+                s=""
+                break
+            #Corrects a situation where removing chars leads to the entire string now having the same character.
+            elif s == s[0]*len(s) and len(s)!=k:
+                s=s
+                break
+            #increment the loop to avoid infinite iteration
+            else:
+                
+                i+=1
+    return s
+
